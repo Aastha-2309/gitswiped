c164e277a489f9a3bea40b19fdd4ca76d3fca6f7
Aastha-2309 <aastha.codes23@gmail.com>
2024-11-25 14:56:25 +0530

    cretaed servers

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
-console.log("starting a new project");
+const express=require("express");
+
+const app=express();
+
+
+app.use("/hello",(req,res)=>{
+    res.send("hi from server hello");
+
+});
+
+
+
+app.use("/test",(req,res)=>{
+    res.send("hi from server");
+
+});
+
+app.listen(7777, () => {console.log("succesful");});
